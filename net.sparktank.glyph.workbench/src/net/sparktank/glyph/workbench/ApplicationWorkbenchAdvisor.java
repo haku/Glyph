69b44e34ecf96cd54f2f293949dbd6c5ba384b0b
はく <haku@vaguehope.com>
2010-12-03 21:58:01 +0000

    Initial import of projects.

---
+++ net.sparktank.glyph.workbench/src/net/sparktank/glyph/workbench/ApplicationWorkbenchAdvisor.java
@@ -0,0 +1,25 @@
+package net.sparktank.glyph.workbench;
+
+import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
+import org.eclipse.ui.application.WorkbenchAdvisor;
+import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
+public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	private static final String PERSPECTIVE_ID = "net.sparktank.glyph.workbench.perspective";
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	@Override
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+	
+	@Override
+	public String getInitialWindowPerspectiveId() {
+		return PERSPECTIVE_ID;
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
---
+++ net.sparktank.glyph/src/net/sparktank/glyph/views/PlayView.java
@@ -0,0 +1,27 @@
+package net.sparktank.glyph.views;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class PlayView extends ViewPart {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	public static final String ID = "net.sparktank.glyph.views.PlayView";
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	ViewPart methods.
+	
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
